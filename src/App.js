3de9bcde3b1b0b9b7f0db652b3969952ebdd73b4
samKallel <samKallel@hotmail.frgit config --global user.email 'samKallel@hotmail.fr>
2023-05-02 22:24:05 +0100

    version finie

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
+// import du component Profile pour l'utiliser dans le component App (son parent)
 import Profile from './Profile/Profile';
-import myImage from './my-image.png';
+// import de l'image qui est stocke dans src/ image donc on donne son path
+import myImage from './img/my-image.png';
 
 function App() {
-  
-  
+   
   return (
     <div className="App">
-      <Profile
+ {/* appel du component Profile on injectant des valeurs a ces props etant donne que App est le parent */}
+      <Profile 
         fullName="Samiha Kallel"
         bio="I am a software engineer"
         profession="Developer"
         age ={46}
       >
+      {/* l'image est le children c'est pour cela on la met entre le tag d'ouverture et de fermeture */}
+      {/* Du style inline!!! */}
        <img src={myImage} alt="profile" style={{ width: '200px', height: '200px', 
         marginRight:'50px', marginLeft:'8%' }}  />
       </Profile>
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -1,29 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// pour importer les type des prosps
+import PropTypes from 'prop-types'; 
 import '../App.css';
 
-const Profile = (props) => {
-  const handleName = () => alert(`My name is ${props.fullName}`);
+const Profile = (props) => { 
+  // on commence par ecrire la fonction qui va etre declenchee une fois le bouton clique
+  const handleName = () => alert(`My name is ${props.fullName}`); 
   return (
-  <div style={{ textAlign: 'center', color:'blueviolet'}}>
+  <div style={{ textAlign: 'center', color:'blueviolet'}}>{/* style inline concernant toute ma div*/}
       <h1>{props.fullName}</h1>
+      <h3>{props.age} ans </h3>
       <h2>{props.profession}</h2>
-    <div className="phButton">
+    <div className="phButton"> {/*je donne un nom a ma classe pour style dans App.css*/}
+    {/* le style concerne limage (children) et le bio pour les mettre en flex */}
       {props.children}
       <p> {props.bio}</p>     
    </div>
-   <button onClick={handleName}>Click me!</button>   
+   <button onClick={handleName}>Click me!</button>   {/* le onClick du bouton recoit la fonction defini en haut*/}
    </div>
   )
 };
+// les types de tout les proprietes (props) de notre component Profile
 Profile.propTypes = {
     fullName: PropTypes.string,
     bio: PropTypes.string,
     profession: PropTypes.string,
     children: PropTypes.element,
     handleName: PropTypes.func,
+    age: PropTypes.number,
   };
-
+// j'ai donne un defaultProps a profession (choix aleatoire) si oublie on aura une valeur a afficher
 Profile.defaultProps = {    
   profession: 'developper',
 };
